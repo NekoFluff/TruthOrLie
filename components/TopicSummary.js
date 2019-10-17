985b4636250d190dcb833d0e8064881c21ccca2e
CrazyFluff <alexnou@gmail.com>
2019-10-16 18:41:59 -0700

    Finished initial topic creation w/o reputation score.

---
+++ components/TopicSummary.js
@@ -0,0 +1,163 @@
+import React, { Component } from "react";
+import { Router } from "../routes";
+import factory from "../ethereum/factory";
+import web3 from "../ethereum/web3";
+
+import {
+  Grid,
+  Segment,
+  Form,
+  Button,
+  Divider,
+  Label,
+  Card,
+  Container,
+  Message
+} from "semantic-ui-react";
+
+class TopicSummary extends Component {
+  state = {
+    loading: false
+  };
+
+  renderTopicCardGroup() {
+    const items = [
+      {
+        header: `Min Investment per User: ${this.props.minimumInvestment} Wei`,
+        description:
+          "This is the minimum amount of wei a user needs to contribute in order to contribute to this Topic. It serves as a barrier to entry for users. Increasing this value encourages users to perform a better investigation on the subject prior to voting.",
+        meta: "How confident should a user be prior to participating?",
+      },
+      {
+        header: `Uptime: ${this.props.hoursAvailable} Hours`,
+        description: "How long should users be able to contribute to this?",
+        meta: ""
+      }
+    ];
+
+    return <Card.Group itemsPerRow={2} centered items={items} stackable />;
+  }
+
+  renderBillingCardGroup() {
+    const items = [
+      {
+        header: `Selected Account: ${this.props.selectedAccount}`,
+        description:
+          "The account that will author this Topic. Any initial investment will be from this account.",
+        meta: "Did you make sure to select the correct account?"
+      },
+      {
+        header: `Initial Topic Value: ${this.props.initialTopicValue} Ether`,
+        description:
+          "Initial monetary investment by you to entice other users to participate. With a higher initial topic value, users will be encouraged to act in the same way as the majority (find the truth).",
+        meta: "How much is this information worth to you?"
+      }
+    ];
+
+    return <Card.Group itemsPerRow={2} centered items={items} stackable />;
+  }
+
+  renderTopicSummary() {
+    return (
+      <Segment raised>
+        <Label
+          as="a"
+          color="red"
+          ribbon
+          onClick={this.props.returnToTopicScreen}
+        >
+          Go Back
+        </Label>
+        <span>Your Topic</span>
+        <Divider></Divider>
+        <Container style={{ marginBottom: "20px" }} textAlign="center">
+          <b>Truth or Lie?</b>
+          <p>"{this.props.topicContent}"</p>
+        </Container>
+
+        {this.renderTopicCardGroup()}
+      </Segment>
+    );
+  }
+
+  renderBillingSummary() {
+    return (
+      <Segment raised>
+        <Label
+          as="a"
+          color="green"
+          ribbon
+          onClick={this.props.returnToBillingScreen}
+        >
+          Go Back
+        </Label>
+        <span>Billing</span>
+        <Divider></Divider>
+
+        {this.renderBillingCardGroup()}
+      </Segment>
+    );
+  }
+
+  onConfirm = async event => {
+    event.preventDefault();
+    try {
+      // Start the loading circle and reset the error message
+      this.setState({ loading: true, errorMessage: "" });
+
+      // Retrieve user accounts and create a new campaign using the CampaignFactory
+      if (this.props.selectedAccount == "") {
+        throw new Error(
+          "No account selected. Please go back to the Billing screen and choose an account."
+        );
+      }
+
+      await factory.methods
+        .createTopic(
+          this.props.topicContent,
+          this.props.minimumInvestment,
+          this.props.hoursAvailable
+        )
+        .send({
+          from: this.props.selectedAccount,
+          value: web3.utils.toWei(this.props.initialTopicValue, "ether")
+        });
+      Router.pushRoute("/");
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ loading: false });
+  };
+
+  render() {
+    return (
+      <Form
+        onSubmit={this.onConfirm}
+        error={!!this.state.errorMessage}
+      >
+        <Grid columns={1} stackable>
+          <Grid.Column>{this.renderTopicSummary()}</Grid.Column>
+
+          <Grid.Column>{this.renderBillingSummary()}</Grid.Column>
+        </Grid>
+
+        <Message
+            error
+            header="Oops! Something went wrong"
+            content={this.state.errorMessage}
+          />
+        <Button
+          primary
+          floated={"right"}
+          loading={this.state.loading}
+          disabled={this.state.loading}
+          style={{ marginTop: "10px" }}
+        >
+          Confirm
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default TopicSummary;
--- pages/topics/new.js
+++ pages/topics/new.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
 import CommonPage from "../../components/CommonPage";
 import NewTopicSteps from "../../components/NewTopicSteps";
-import { Router } from "../../routes";
-import factory from "../../ethereum/factory";
 import web3 from "../../ethereum/web3";
-import { Form, Button, Input, Message } from "semantic-ui-react";
+import { Form, Button, Message, Dropdown } from "semantic-ui-react";
+import TopicSummary from "../../components/TopicSummary";
 
 class NewTopic extends Component {
   state = {
-    minimumInvestment: "",
-    topicContent: "",
-    hoursAvailable: "",
-    initialTopicValue: "",
     errorMessage: "",
+    phase: 1,
+    topicContent: "",
+    minimumInvestment: "",
+    hoursAvailable: "",
+    
+    // Billing
+    accountsList: [],
+    selectedAccount: "",
+    initialTopicValue: "",
+
+    // Billing errors
+    accountSelectionError: "",
+
+    // Topic-specific errors
     topicError: "",
     minimumInvestmentError: "",
     activeTimeError: "",
@@ -20,48 +29,125 @@ class NewTopic extends Component {
     loading: false
   };
 
-  onSubmit = async event => {
-    event.preventDefault();
-    //TODO: Double confirmation button OR Popup https://react.semantic-ui.com/modules/popup/
-    try {
+  async componentDidMount() {
+    console.log("Component mounted:", this.state)
+    await this.retrieveAccounts();
+  }
 
-      // Start the loading circle and reset the error message
-      this.setState({ loading: true, errorMessage: "" });
+  renderConfirmationForm() {
+    return (
+      <React.Fragment>
+        <h2>Is this right?</h2>
+        <TopicSummary
+          returnToTopicScreen={() => {
+            this.setState({ phase: 1 });
+          }}
+          returnToBillingScreen={() => {
+            this.setState({ phase: 2 });
+          }}
+          {...this.state}
+        />
+      </React.Fragment>
+    );
+  }
 
-      // Retrieve user accounts and create a new campaign using the CampaignFactory
-      const accounts = await web3.eth.getAccounts();
-      if (accounts.length == 0) {
-        throw new Error("No available accounts were found. Please make sure to connect your wallet using Metamask!");
+  retrieveAccounts = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    let accountsList = accounts.map((acc) => {
+      return {
+        key: acc,
+        text: acc,
+        value: acc
       }
-      
-      await factory.methods
-      .createTopic(
-        this.state.topicContent,
-        this.state.minimumInvestment,
-        this.state.hoursAvailable
-        )
-        .send({
-          from: accounts[0],
-          value: web3.utils.toWei(this.state.initialTopicValue, "ether")
-        });
-      Router.pushRoute("/");
+    })
+
+    if (accounts.length == 0) {
+      this.setState({ accountSelectionError: "There are no available accounts." })
+    }
+
+    this.setState({ accountsList })
+  }
+
+  renderBillingForm() {
+    return (
+      <React.Fragment>
+        <h2>What account would you like to use?</h2>
+        <Form onSubmit={this.onBillingNext} error={!!this.state.errorMessage}>
+          <Form.Field>
+            <Dropdown
+              error={this.state.accountSelectionError != ""}
+              placeholder="Select an Account"
+              fluid
+              selection
+              options={this.state.accountsList}
+              onChange={(event, {value}) => {this.setState({ accountSelectionError: "", selectedAccount: value })}}
+            />
+          </Form.Field>
+          <Form.Input
+            error={
+              this.state.initalTopicValueError != "" && {
+                content: this.state.initalTopicValueError,
+                pointing: "below"
+              }
+            }
+            fluid
+            required
+            label="Initial Topic Value (ether)"
+            labelPosition="right"
+            placeholder="How much do you want to invest in this topic? (Entice more users to participate)"
+            type="number"
+            value={this.state.initialTopicValue}
+            onChange={event => {
+              this.setState({ initialTopicValue: event.target.value });
+            }}
+          />
+
+
+          <Message
+            error
+            header="Oops! Something went wrong"
+            content={this.state.errorMessage}
+          />
+          <Button
+            primary
+            floated={"right"}
+            loading={this.state.loading}
+            disabled={this.state.loading}
+          >
+            Next
+          </Button>
+        </Form>
+      </React.Fragment>
+    );
+  }
+
+  onBillingNext = async event => {
+    event.preventDefault();
+    try {
+      if (this.state.selectedAccount == "") {
+        this.setState({ accountSelectionError: "You must select an account before proceeding"});
+        throw new Error("You must select an account before proceeding")
+      }
+      this.setState({ phase: 3, errorMessage: "" });
     } catch (err) {
       this.setState({ errorMessage: err.message });
     }
     this.setState({ loading: false });
   };
 
-  render() {
+  renderNewTopicForm() {
     return (
-      <CommonPage>
-        <NewTopicSteps active={1} />
-        <h3>Let's talk about something!</h3>
-        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
+      <React.Fragment>
+        <h2>Let's talk about something!</h2>
+        <Form onSubmit={this.onFormNext} error={!!this.state.errorMessage}>
           <Form.Input
-            error={this.state.topicError != "" && {
-              content: this.state.topicError,
-              pointing: "below"
-            }}
+            error={
+              this.state.topicError != "" && {
+                content: this.state.topicError,
+                pointing: "below"
+              }
+            }
             required
             fluid
             label="Topic"
@@ -73,10 +159,12 @@ class NewTopic extends Component {
             }}
           />
           <Form.Input
-            error={this.state.minimumInvestmentError != "" && {
-              content: this.state.minimumInvestmentError,
-              pointing: "below"
-            }}
+            error={
+              this.state.minimumInvestmentError != "" && {
+                content: this.state.minimumInvestmentError,
+                pointing: "below"
+              }
+            }
             fluid
             required
             label="Minimum Investment per Voter (wei)"
@@ -88,34 +176,20 @@ class NewTopic extends Component {
             }}
           />
           <Form.Input
-            error={this.state.activeTimeError != "" && {
-              content: this.state.activeTimeError,
-              pointing: "below"
-            }}
+            error={
+              this.state.activeTimeError != "" && {
+                content: this.state.activeTimeError,
+                pointing: "below"
+              }
+            }
             fluid
             required
-            label="Active Time (Hours)"
+            label="Active Time (hours)"
             placeholder="How long should the voting period last?"
             type="number"
             value={this.state.hoursAvailable}
             onChange={event => {
               this.setState({ hoursAvailable: event.target.value });
-            }}
-          />
-          <Form.Input
-            error={this.state.initalTopicValueError != "" && {
-              content: this.state.initalTopicValueError,
-              pointing: "below"
-            }}
-            fluid
-            required
-            label="Initial Topic Value (ether)"
-            labelPosition="right"
-            placeholder="How much do you want to invest in this topic? (Entice more users to participate)"
-            type="number"
-            value={this.state.initialTopicValue}
-            onChange={event => {
-              this.setState({ initialTopicValue: event.target.value });
             }}
           />
 
@@ -133,6 +207,47 @@ class NewTopic extends Component {
             Next
           </Button>
         </Form>
+      </React.Fragment>
+    );
+  }
+
+  onFormNext = async event => {
+    event.preventDefault();
+    //TODO: Double confirmation button OR Popup https://react.semantic-ui.com/modules/popup/
+    try {
+      await this.retrieveAccounts();
+      this.setState({ phase: 2, errorMessage: "" });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ loading: false });
+  };
+
+  renderContent() {
+    if (this.state.phase == 1) return this.renderNewTopicForm();
+    else if (this.state.phase == 2) return this.renderBillingForm();
+    else return this.renderConfirmationForm();
+  }   
+
+  render() {
+    return (
+      <CommonPage>
+        <NewTopicSteps active={this.state.phase}/>
+        {this.state.phase > 1 && (
+          <Button
+            // style={{ marginBottom: "10px"} }
+            primary
+            // loading={this.state.loading}
+            disabled={this.state.loading}
+            onClick={() => {
+              this.setState({ phase: Math.max(1, this.state.phase - 1) });
+            }}
+          >
+            Back
+          </Button>
+        )}
+
+        {this.renderContent()}
       </CommonPage>
     );
   }
