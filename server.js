f15aae3894f36d053640c0b8c1427309affb92dd
Alex <alexnou@gmail.com>
2020-01-27 16:31:44 -0700

    https fix?

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.prepare().then(() => {
 
   // Redirect to https on production
   if (process.env.NODE_ENV === "production") {
-    app.use((req, res, next) => {
+    server.use((req, res, next) => {
       if (req.header("x-forwarded-proto")) {
         res.redirect(`https://${req.header("host")}${req.url}`);
       } else {
