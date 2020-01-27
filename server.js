041a62e276f39c5a30cb42ba8bf192e4d59ad14c
Alex <alexnou@gmail.com>
2020-01-27 16:07:35 -0700

    Added redirect to https in server.js

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
 const next = require("next");
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = parseInt(process.env.PORT, 10) || 3000;
 const routes = require("./routes");
 const app = next({
   dev: process.env.NODE_ENV !== "production"
 });
 const handler = routes.getRequestHandler(app);
-const HomormorphicEncryption = {a: () => {return 1;}};
+const HomormorphicEncryption = {
+  a: () => {
+    return 1;
+  }
+};
 
 const handle = app.getRequestHandler();
 
 // Create server
-const express = require('express')
+const express = require("express");
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/a', (req, res) => {
+  // Redirect to https on production
+  if (process.env.NODE_ENV === "production") {
+    app.use((req, res, next) => {
+      if (req.header("x-forwarded-proto")) {
+        res.redirect(`https://${req.header("host")}${req.url}`);
+      } else {
+        next();
+      }
+    });
+  }
+
+  // Test
+  server.get("/a", (req, res) => {
     console.log("Homomorhpic Encryption A");
     console.log(HomormorphicEncryption.a());
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
 
   // server.all('*', (req, res) => {
   //   console.log("Homomorhpic Encryption STAR");
@@ -26,10 +42,9 @@ app.prepare().then(() => {
   //   return handle(req, res)
   // })
 
+  // Handler
   server.use(handler).listen(port, err => {
     if (err) throw err;
     console.log(`Ready on localhost:${port}`);
   });
-})
-
-
+});
