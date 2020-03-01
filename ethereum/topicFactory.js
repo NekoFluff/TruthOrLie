5b156f61081e99058f99ffe4ab39f7ad117631da
CrazyFluff <alexnou@gmail.com>
2020-03-01 16:36:46 -0700

    Mainnet build.

--- ethereum/topicFactory.js
+++ ethereum/topicFactory.js
@@ -3,8 +3,8 @@ import TopicFactory from "./build/TopicFactory.json";
 
 const instance = new web3.eth.Contract(
   TopicFactory.abi,
-  "0x82aBD8D15f8B0bcF93E1b77e2AfAF1d0dd7f2C21" // Rinkeby
-  // "0x7fD78dD9eF88A712e2DB9851358aE315564c5957"
+  //"0x82aBD8D15f8B0bcF93E1b77e2AfAF1d0dd7f2C21" // Rinkeby
+  "0x7fD78dD9eF88A712e2DB9851358aE315564c5957"
 );
 
 export default instance;
